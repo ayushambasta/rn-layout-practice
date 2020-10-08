9511313d688ba5e58e9620046383ea751d666479
Ayush Saurav <theasaurav@gmail.com>
2020-10-08 16:21:09 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './navigation/Navigation'
+
+export default function App() {
+  return (
+    <Navigation />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
